194522614f2a214457b61fbadfb41cdeab70364c
Mehmet Ali Kürkçüoğlu <115497918+malikurkcuoglu@users.noreply.github.com>
2025-05-23 07:23:54 +0300

    Add calculating and displaying
    Take first numbers and operator when an operator is pressed. If it is the
    first time wait for second number. If it is pressed before calculate and
    display with new operator. When "=" pressed calculate and display. Need to
    clear the display when user enters a new number again.

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let num1, num2, operator;
+let num1, num2, operator, displayNum;
 const display = document.querySelector(".text");
+const secondText = document.querySelector(".second-text");
 const digits = document.querySelectorAll(".digit");
 const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
+let isPressed = false;
 
 digits.forEach((digit) =>
-  digit.addEventListener(
-    "click",
-    (e) => (display.textContent += e.target.textContent)
-  )
+  digit.addEventListener("click", (e) => {
+    display.textContent += e.target.textContent;
+    displayNum = display.textContent;
+  })
 );
-
-let displayNum = Number(display.textContent);
 
 const add = function (a, b) {
   return a + b;
@@ -28,22 +30,53 @@ const divide = function (a, b) {
   if (b !== 0) return a / b;
   else return "ERR";
 };
-
 const operate = function (a, b, op) {
-  Number(a);
-  Number(b);
   switch (op) {
     case "+":
-      add(a, b);
+      return add(Number(a), Number(b));
       break;
     case "-":
-      subtract(a, b);
+      return subtract(Number(a), Number(b));
       break;
     case "/":
-      divide(a, b);
+      return divide(Number(a), Number(b));
       break;
     case "*":
-      multiply(a, b);
+      return multiply(Number(a), Number(b));
       break;
   }
 };
+
+operators.forEach((op) =>
+  op.addEventListener("click", (e) => {
+    if (isPressed) {
+      num2 = displayNum;
+      let result = operate(num1, num2, operator);
+      num1 = result;
+      operator = e.target.textContent === "X" ? "*" : e.target.textContent;
+      secondText.textContent = `${result}${operator}`;
+      display.textContent = "";
+      num2 = null;
+    } else {
+      num1 = displayNum;
+      display.textContent = "";
+      operator = e.target.textContent === "X" ? "*" : e.target.textContent;
+      secondText.textContent = `${num1} ${operator}`;
+      isPressed = true;
+    }
+  })
+);
+equals.addEventListener("click", () => {
+  isPressed = false;
+  num2 = displayNum;
+  secondText.textContent = "";
+  display.textContent = operate(num1, num2, operator);
+});
+clear.addEventListener("click", () => {
+  num1 = null;
+  num2 = null;
+  isPressed = false;
+  display.textContent = "";
+  secondText.textContent = "";
+  operator = "";
+});
